455f5352dbb354262d5066f6b8f1ca6a2ff32029
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-13 16:54:59 -0500

    Add icons to the post to edit, delete and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   # GET /posts/1
