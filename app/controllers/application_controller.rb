c410996c5fe1aad7dd5ec0670b3804381e4e928d
Giordano Dìaz <diazgiordano@gmail.com>
2020-10-20 14:27:40 -0500

    Create helpers methods, move logic from views and solve problem with users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  helper_method :signed_in?
-
-  def signed_in?
-    !current_user.nil?
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,18 @@ module ApplicationHelper
   # rubocop: enable Style/HashSyntax
 
   def signed_in?
-    !current_user.nil?
+    if user_signed_in?
+      ("#{link_to current_user.name, edit_user_registration_path, class: 'btn text-light m-2'}"+"#{link_to 'Logout', destroy_user_session_path, method: :delete, class: 'btn text-light m-2'}").html_safe
+    else
+      ("#{link_to 'Sing In', new_user_session_path, class: 'btn text-light m-2'}"+"#{link_to 'Sing Up', new_user_registration_path, class: 'btn text-light m-2'}").html_safe
+    end
+  end
+
+  def profileCol
+    if user_signed_in? 
+      (render 'profile').html_safe
+    else
+      (render 'trends').html_safe
+    end
   end
 end
